e1fe7cb47391fe25b838951a5a0bf8511407499b
Mark Druffel <mark.druffel@gmail.com>
2024-02-12 16:28:57 -0800

    Rebasing to create dev

--- src/kedro_ibis/pipelines/demo/nodes.py
+++ src/kedro_ibis/pipelines/demo/nodes.py
@@ -2,3 +2,9 @@
 This is a boilerplate pipeline 'demo'
 generated using Kedro 0.19.2
 """
+def write_delta(**kwargs):
+    path = "data/bronze/managed/"
+    for name, df in kwargs.items():
+        filepath = f"{path}{name}"
+        df.write.mode("overwrite").format("delta").save(filepath)
+    return filepath
--- src/kedro_ibis/pipelines/demo/pipeline.py
+++ src/kedro_ibis/pipelines/demo/pipeline.py
@@ -4,16 +4,16 @@ generated using Kedro 0.19.2
 """
 
 from kedro.pipeline import Pipeline, pipeline, node
-from .nodes import *
+from .nodes import write_delta
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
-            #node(
-            #    func=None,
-            #    inputs="source_yellow@spark",
-            #    outputs="raw_yellow@spark",
-            #    name="raw_yellow"
-            #)
+            node(
+                func=write_delta,
+                inputs={"yellow_trips": "yellow_trips_spark"},
+                outputs=None,
+                name="delta"
+            )
 
         ]
     )
